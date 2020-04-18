ce29a5fd15632c55d782a51fd578db2dac75614f
Evan <ahmeed.3826@gmail.com>
2020-04-18 12:00:30 +0600

    initial

---
+++ app/src/main/java/com/example/delimantrackorder/RunClass.java
@@ -0,0 +1,14 @@
+package com.example.delimantrackorder;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RunClass extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rider_info_after);
+    }
+}
